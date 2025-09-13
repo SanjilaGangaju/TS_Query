09cb7c51552419d91657ebd7293ee2193b8fb508
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-09-13 08:50:44 +0545

    get all products with tanstack query

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,25 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useEffect, useState } from 'react'
 
 const ProductList = () => {
-    const [products, setProducts] = useState([]);
-    const api = 'https://fakestoreapi.com'
-    async function fetchProducts(){
-        const res = await fetch(`${api}/products`)
-        const data = await res.json()
-        console.log(data)
-        setProducts(data)
-    }
-   
     
-    useEffect(()=>{
-        
-      fetchProducts()
-    }, [])
+    const api = 'https://fakestoreapi.com'
+
+    async function fetchProducts(){
+      const res = await fetch(`${api}/products`)
+      const  data = await res.json()
+      return data
+
+    }
+    
+    const {data} = useQuery({queryKey:['products'],
+      queryFn: fetchProducts,
+    })
     
   return (
     <div>
       Product List 
-      {products.map(item=><p key={item.title}>{item.title}</p>)}
+      {data?.map(item=><p key={item.title}>{item.title}</p>)}
     </div>
   )
 }
