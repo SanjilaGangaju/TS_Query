cc63588f1679bd814ec17c62ae84e6c4ee057762
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-09-13 12:01:25 +0545

    managing server states

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,25 +1,46 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useEffect, useState } from 'react'
+
+import { ClipLoader } from "react-spinners";
+
 
 const ProductList = () => {
     
     const api = 'https://fakestoreapi.com'
 
     async function fetchProducts(){
-      const res = await fetch(`${api}/products`)
+      try{
+          const res = await fetch(`${api}/products`)
+    
       const  data = await res.json()
       return data
+      }
+      catch(error){
+        console.log(error) 
+      }
+    
+      
 
     }
     
-    const {data} = useQuery({queryKey:['products'],
+    const {data, isLoading ,error, isSuccess, isFetching} = useQuery({queryKey:['products'],
       queryFn: fetchProducts,
     })
+    if (isLoading){
+      return <ClipLoader></ClipLoader>
+    }
+    if (error){
+      return <div>Error: {error.message}</div>
+    }
+    if (isFetching){
+      return <div>fetching</div>
+    }
     
   return (
     <div>
+
       Product List 
-      {data?.map(item=><p key={item.title}>{item.title}</p>)}
+      {isSuccess&& data?.map(item=><p key={item.title}>{item.title}</p>)
+}
     </div>
   )
 }
