8b5691171e80f9ec5466caa69e5959f1d93bb150
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-09-13 08:15:31 +0545

    setup for ecommerce project

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { MainLayout } from "./components/Layout/MainLayout"
+import React from 'react'
 
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import FetchIndv from "./UI/FetchIndv"
-import { fetchPosts } from "./API/api"
-import { useState } from "react"
-import Posts from "./components/Posts"
-
-const App = ()=>{
- 
-        const [isMounted, setIsMounted] = useState(false);
-
-  return(
-    <>
-    <button onClick={()=>setIsMounted((prev)=>!prev)}>Toggle </button>
-    {isMounted && <Posts/>}
-    </>
+const App = () => {
+  return (
+    <div>
+      Ecommerce Store
+    </div>
   )
- 
-  
 }
+
 export default App
